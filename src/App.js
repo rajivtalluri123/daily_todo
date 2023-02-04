08866a7c960682d3051b59278b2f583dd1493530
rajiv.talluri <rajiv.talluri@xperi.com>
2023-02-04 11:59:37 -0500

    add counts for the last 10 days-- This commit is not handling clearing local storage after 10 days- Add it later if needed

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import DisplayList from './components/DisplayList'
 
 function App() {
 
+
     const dailyRoutineTodos = [
-        {  task: "Minoxidil", completed: false },
+        { task: "Leet Code", completed: false},
         {  task: "Juice + BF", completed: false },
+        {  task: "Run", completed: false },
+        { task: "Isha Kriya-1", completed: false},
         {  task:  "Nuts + dry fruits", completed: false},
-        {  task:  "Run", completed: false},
+        {  task:  "Minoxidil", completed: false},
+        { task: "Xperi", completed: false},
         {  task: "Lunch", completed: false },
         {  task: "Vitamins + finasteride", completed: false},
         {  task:"CrossFit", completed: false},
+        { task:"Isha Kriya-2", completed: false},
         {  task: "Fruits", completed: false },
         {  task: "Laser", completed: false},
         {  task: "Shower", completed: false},
-        {  task: "spray + minoxidil + brush + vitamins", completed: false},
-        {  task: "Visa and EB3", completed: false}
+        {  task: "spray + minoxidil + brush + eye-cream", completed: false},
+        {  task: "Vitamins", completed: false},
+        {  task: "App development", completed: false}
     ];
 
     const dailyXperiTodos = [
@@ -27,12 +33,11 @@ function App() {
     const dailyAppsTodos = [
         {  task: "check for improvement and add fetures", completed: false },
     ];
-
     return (
     <div>
-        <DisplayList todos={dailyRoutineTodos}  header = "My Daily Routine"/>
-        <DisplayList todos={dailyXperiTodos} header = "My Xperi Routine Today" />
-        <DisplayList todos={dailyAppsTodos} header = "My Other Apps Routine Today" />
+        <DisplayList todos={dailyRoutineTodos}  header = "My Daily Routine" decade = "23.2.1"/>
+        <DisplayList todos={dailyXperiTodos} header = "My Xperi Todo Today" />
+        <DisplayList todos={dailyAppsTodos} header = "My Other Apps Today Today" />
     </div>
 
     );
--- src/components/DisplayList.js
+++ src/components/DisplayList.js
@@ -1,12 +1,76 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import '../App.css';
 
 const DisplayList = (props) => {
+    //count to track how time I did leet code in last 10 days
+    const [leetCodeCount, setLeetCodeCount] = useState(() => {
+        const initialCount = localStorage.getItem('leetCodeCount') || 0;
+        return parseInt(initialCount, 10);
+    });
+    useEffect(() => {
+        localStorage.setItem('leetCodeCount', leetCodeCount);
+    }, [leetCodeCount]);
+
+    //count to track how time I did run in last 10 days
+    const [runCount, setRunCount] = useState(() => {
+        const initialCount = localStorage.getItem('runCount') || 0;
+        return parseInt(initialCount, 10);
+    });
+    useEffect(() => {
+        localStorage.setItem('runCount', runCount);
+    }, [runCount]);
+
+    //count to track how xperi sessions I did run in last 10 days
+    const [xperiCount, setXperiCount] = useState(() => {
+        const initialCount = localStorage.getItem('xperiCount') || 0;
+        return parseInt(initialCount, 10);
+    });
+    useEffect(() => {
+        localStorage.setItem('xperiCount', xperiCount);
+    }, [xperiCount]);
+
+    //count to track how App dev sessions I did run in last 10 days
+    const [appDevCount, setAppDevCount] = useState(() => {
+        const initialCount = localStorage.getItem('appDevCount') || 0;
+        return parseInt(initialCount, 10);
+    });
+    useEffect(() => {
+        localStorage.setItem('appDevCount', appDevCount);
+    }, [appDevCount]);
+
+
     const [todos, setTodos] = useState(props.todos);
     const header = useState(props.header)
-    const handleComplete = task => {
+    const handleComplete = (event, task) => {
         setTodos(todos.map(todo => {
             if (todo.task === task) {
+                if(task == 'Leet Code')
+                    setLeetCodeCount(leetCodeCount +1);
+                if(task == 'Run')
+                    setRunCount(runCount +1);
+                if(task == 'Xperi') {
+                    console.log(event.target.innerHTML);
+                    if (event.target.innerHTML == 'Complete 1') {
+                        setXperiCount(xperiCount + 1);
+                    }
+                    if (event.target.innerHTML == 'Complete 2') {
+                        setXperiCount(xperiCount + 2);
+                    }
+                    if (event.target.innerHTML == 'Complete 3') {
+                        setXperiCount(xperiCount + 3);
+                    }
+                }
+                if(task == 'App development') {
+                    if(event.target.innerHTML == 'Complete 1') {
+                        setAppDevCount(appDevCount +1);
+                    }
+                    if(event.target.innerHTML == 'Complete 2') {
+                        setAppDevCount(appDevCount +2);
+                    }
+                    if(event.target.innerHTML == 'Complete 3') {
+                        setAppDevCount(appDevCount +3);
+                    }
+                }
                 return { task, completed: true };
             }
             return todo;
@@ -43,7 +107,18 @@ const DisplayList = (props) => {
                 {activeTodos.map((todo, index) => (
                     <li key={index}>
                         {todo.task}
-                        <button onClick={() => handleComplete(todo.task)}>Complete</button>
+                        {/*code to display no of pomodoro sessions done in that day for work*/}
+                        {todo.task == 'Xperi' || todo.task == 'App development' ? <div> <button onClick={(event) => handleComplete(event, todo.task)}>Complete 1</button>
+                            <button onClick={(event) => handleComplete(event, todo.task)}>Complete 2</button>
+                            <button onClick={(event) => handleComplete(event, todo.task)}>Complete 3</button> </div> : (<button onClick={(event) => handleComplete(event, todo.task)}>Complete</button>)
+                        }
+
+                        {/*code to display counts*/}
+                        {todo.task == 'Leet Code' ?  <p> U have done LeetCoding or SystemDesigning <span style={{ color: 'red', fontWeight: 'bolder' }}>  {leetCodeCount} </span> times in this {props.decade} </p>
+                            : todo.task == 'Run' ? <p> U have done Runing <span style={{ color: 'red', fontWeight: 'bolder' }}> {runCount} </span> times in this {props.decade} </p>
+                                :todo.task == 'Xperi'? <p> U have done <span style={{ color: 'red', fontWeight: 'bolder' }}> {xperiCount} </span> sessions in this {props.decade}</p>
+                                    :todo.task == 'App development'? <p> U have done <span style={{ color: 'red', fontWeight: 'bolder' }}>{appDevCount}</span> sessions in this {props.decade}</p>:"" }
+
                     </li>
                 ))}
             </ol>
